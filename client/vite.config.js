98613c39ff9f367d810e42b4dc5986aca2d362c3
Minh Thịnh <111744097+MinhThinh00@users.noreply.github.com>
2024-06-26 03:04:45 +0700

    fix /

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://estate-sigma-three.vercel.app/',
+        target: 'https://estate-sigma-three.vercel.app',
         secure: false,
       },
     },
